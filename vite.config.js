23bf4afc402754aa96ec5a91a8f09a5c1c54226f
Mouli Chandramouli <mchandramouli@gmail.com>
2024-12-11 23:06:09 -0700

    Update Vercel configuration

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
-  base: './',  // Add this line
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
+  base: '/',
 })
